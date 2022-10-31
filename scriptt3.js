81274c084fc0f3593f902580c465f3ff2e0250b1
Burak Kahraman <burak.kahramann16@gmail.com>
2022-10-31 15:26:24 +0300

    Initial commit

---
+++ scriptt3.js
@@ -0,0 +1,64 @@
+
+function _9(){
+document.getElementById("sonuc").value+=9;
+}
+
+function _8(){
+    document.getElementById("sonuc").value+=8;
+}
+function _7(){
+document.getElementById("sonuc").value+=7;
+}
+function _6(){
+document.getElementById("sonuc").value+=6;
+}
+function _5(){
+document.getElementById("sonuc").value+=5;
+}
+function _4(){
+document.getElementById("sonuc").value+=4;
+}
+function _3(){
+document.getElementById("sonuc").value+=3;
+}
+function _2(){
+document.getElementById("sonuc").value+=2;
+}
+function _1(){
+document.getElementById("sonuc").value+=1;
+}
+function _0(){
+document.getElementById("sonuc").value+=0;
+}
+function topla(){
+document.getElementById("sonuc").value+='+';
+
+}
+
+function carp(){
+document.getElementById("sonuc").value+='*';
+    
+}
+function bol(){
+document.getElementById("sonuc").value+='/';
+      
+}
+function cikar(){
+document.getElementById("sonuc").value+='-';
+        
+}
+function yuzde(){
+  document.getElementById("sonuc").value+='%';
+
+}
+function nokta(){
+  document.getElementById("sonuc").value+='.';
+
+}
+function _c(){
+  document.getElementById("sonuc").value=' ';
+  }
+function esittir(){
+  document.getElementById("sonuc").value=eval(document.getElementById("sonuc").value);
+  
+}
